be5236f9164eaf25f3748bb0b11c1a1460cd83f0
dwikialfitrah <dwiki.alfitrah070101@gmail.com>
2026-01-21 22:03:56 +0700

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,31 +1,16 @@
-const firebaseConfig = {
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// CONFIG FIREBASE (PASTE DARI CONSOLE)
 const firebaseConfig = {
   apiKey: "AIzaSyAUdkRKithy90zkDMa14nfjQne5FxmkhZM",
   authDomain: "website-satpel-pariaman.firebaseapp.com",
   projectId: "website-satpel-pariaman",
-  storageBucket: "website-satpel-pariaman.firebasestorage.app",
-  messagingSenderId: "857253501191",
-  appId: "1:857253501191:web:a169ce2bebf7267d124da2",
-  measurementId: "G-61PL0G1C5J"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-};
-
+// INIT
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
-const adminEmail = "admin@email.com"; // GANTI EMAIL ADMIN
+// EMAIL ADMIN
+const adminEmail = "dwiki.alfitrah070101@gmail.com";
 
 function login() {
   const email = document.getElementById("email").value;
