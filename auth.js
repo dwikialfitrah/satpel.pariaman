2e3404f82e86c16c8817dc51ff1128bd2ca2ee64
dwikialfitrah <dwiki.alfitrah070101@gmail.com>
2026-01-22 20:08:00 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,38 +1,19 @@
-// CONFIG FIREBASE (PASTE DARI CONSOLE)
-const firebaseConfig = {
-  apiKey: "AIzaSyAUdkRKithy90zkDMa14nfjQne5FxmkhZM",
-  authDomain: "website-satpel-pariaman.firebaseapp.com",
-  projectId: "website-satpel-pariaman",
-};
-
-// INIT
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-// EMAIL ADMIN
-const adminEmail = "dwiki.alfitrah070101@gmail.com";
-
 function login() {
   const email = document.getElementById("email").value;
-  const pass = document.getElementById("password").value;
+  const password = document.getElementById("password").value;
 
-  auth.signInWithEmailAndPassword(email, pass)
-    .then((userCredential) => {
-      const user = userCredential.user;
-
-      if (user.email === adminEmail) {
-        window.location.href = "dashboard-admin.html";
-      } else {
-        window.location.href = "dashboard-user.html";
-      }
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.replace("dashboard-user.html");
     })
-    .catch(err => {
-      document.getElementById("error").innerText = err.message;
+    .catch(() => {
+      document.getElementById("error").innerText =
+        "Email atau password salah";
     });
 }
 
 function logout() {
-  auth.signOut().then(() => {
-    window.location.href = "index.html";
+  firebase.auth().signOut().then(() => {
+    window.location.replace("index.html");
   });
 }
