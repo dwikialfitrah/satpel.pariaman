bbc4fe1fac5c3cdc119357bc3eb32f11b24d3315
dwikialfitrah <dwiki.alfitrah070101@gmail.com>
2026-02-18 22:33:05 +0700

    Update guard.js

--- guard.js
+++ guard.js
@@ -5,26 +5,21 @@ const adminEmails = [
 firebase.auth().onAuthStateChanged(function(user) {
 
   if (!user) {
-    window.location.href = "index.html";
+    window.location.replace("index.html");
     return;
   }
 
   const isAdmin = adminEmails.includes(user.email);
   const currentPage = window.location.pathname;
 
-  // Jika user biasa buka dashboard-admin
-  if (currentPage.includes("dashboard-admin") && !isAdmin) {
-    window.location.href = "dashboard-user.html";
+  if (currentPage.endsWith("dashboard-admin.html") && !isAdmin) {
+    window.location.replace("dashboard-user.html");
     return;
   }
 
-  // Jika admin buka dashboard-user
-  if (currentPage.includes("dashboard-user") && isAdmin) {
-    window.location.href = "dashboard-admin.html";
+  if (currentPage.endsWith("dashboard-user.html") && isAdmin) {
+    window.location.replace("dashboard-admin.html");
     return;
   }
 
-  // Jika lolos semua pengecekan, tampilkan halaman
-  document.body.style.display = "block";
 });
-
