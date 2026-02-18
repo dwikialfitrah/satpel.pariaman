eb996fba5f680d100c0606d68527b58cca0d7916
dwikialfitrah <dwiki.alfitrah070101@gmail.com>
2026-02-18 22:02:11 +0700

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,5 +1,36 @@
-firebase.auth().onAuthStateChanged(user => {
+// ===============================
+// DAFTAR EMAIL ADMIN (HARUS SAMA DENGAN auth.js)
+// ===============================
+const adminEmails = [
+  "admin1@email.com",
+  "admin2@email.com"
+];
+
+
+// ===============================
+// GUARD PROTECTION
+// ===============================
+firebase.auth().onAuthStateChanged(function(user) {
+
   if (!user) {
     window.location.replace("index.html");
+    return;
   }
+
+  const currentPage = window.location.pathname;
+
+  const isAdmin = adminEmails.includes(user.email);
+
+  // Jika user biasa mencoba akses dashboard admin
+  if (currentPage.includes("dashboard-admin.html") && !isAdmin) {
+    window.location.replace("dashboard-user.html");
+    return;
+  }
+
+  // Jika admin membuka dashboard user
+  if (currentPage.includes("dashboard-user.html") && isAdmin) {
+    window.location.replace("dashboard-admin.html");
+    return;
+  }
+
 });
